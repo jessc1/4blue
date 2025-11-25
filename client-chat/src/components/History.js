d05b327a14444316d1e707e008cbf5215590872d
jessc1 <mmariajessica@gmail.com>
2025-11-25 16:37:34 -0300

    fix history mgs

--- client-chat/src/components/History.js
+++ client-chat/src/components/History.js
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import axiosService from "../helpers/axios";
 
-    function History() {
-        const [messages, setMessages] = useState([]);
-        const [loading, setLoading] = useState(true);
-        const [error, setError] = useState(null);
+function History() {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-        useEffect(() => {
-            axiosService.get('http://localhost:8000/api/chat/')
-                .then(res => {
-                    setMessages(res.data);
-                    setLoading(false);
-                })
-                .catch(error => {
-                    setError(error);
-                    setLoading(false);
-                });
-        }, []);
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const response = await axiosService.get("http://localhost:8000/api/chat/")        
+        const data = response.data;
+        console.log('data', data);
+        setMessages(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        if (loading) return <p>Loading messages...</p>;
-        if (error) return <p>Error loading messages: {error.message}</p>;
+    fetchMessages();
+  }, []);
 
-        return (
-       <div >
-        {messages.results && messages.results.map((msg) => (
-                <div key={msg.id} style={{ marginBottom: '10px' }}>
-                    <p><strong>User:</strong> {msg.user}</p>
-                    {msg.message && <p><strong>Bot:</strong> {msg.response}</p>}
-                </div>
+  if (loading) return <p>Loading messages...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Messages</h2>
+      <ul>
+            {messages.map(message => (
+                <li key={message.id}>
+                    <span>User:{message.user.username}</span>
+                    <span>Mensagem:{message.message}:</span>
+                    <span>Chat:{message.response}</span>
+                </li>
             ))}
-           
-        </div>
-        )
-    }
+      </ul>
+    </div>
+  );
+}
 
-    export default History;
+export default History;
